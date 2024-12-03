d36ecc0280a1654793f01c30c2f01c11fb929441
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2024-12-03 00:04:12 +0000

    Day 02 done

--- src/aoc/day02.rs
+++ src/aoc/day02.rs
@@ -60,6 +60,42 @@ fn get_num_safe_reports(input_file: &str) -> u32 {
     num_safe_reports
 }
 
+fn get_num_safe_reports_with_prob_damp(input_file: &str) -> u32 {
+    let input = parse_input(input_file);
+
+    let mut num_safe_reports = 0;
+
+    for report in input.reports {
+        for n in 0..report.len() {
+            let filtered_report: Vec<i32> = report
+                .iter()
+                .enumerate()
+                .filter(|&(i, _)| i != n)
+                .map(|(_, n)| n)
+                .cloned()
+                .collect();
+            let gradients: Vec<i32> = filtered_report
+                .windows(2)
+                .map(|elems| {
+                    if let [left, right] = &elems[..] {
+                        *right - *left
+                    } else {
+                        panic!()
+                    }
+                })
+                .collect();
+            if (is_all_increasing(&gradients) || is_all_decreasing(&gradients))
+                && is_all_adj_diff_in_limits(&gradients)
+            {
+                num_safe_reports += 1;
+                break;
+            }
+        }
+    }
+
+    num_safe_reports
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -70,7 +106,20 @@ mod tests {
     }
 
     #[test]
-    fn test_get_total_distance() {
+    fn test_get_num_safe_reports() {
         assert_eq!(686, get_num_safe_reports("input/day02.txt"));
+    }
+
+    #[test]
+    fn test_get_num_safe_reports_with_prob_damp_test01() {
+        assert_eq!(
+            4,
+            get_num_safe_reports_with_prob_damp("input/day02_test01.txt")
+        );
+    }
+
+    #[test]
+    fn test_get_num_safe_reports_with_prob_damp() {
+        assert_eq!(717, get_num_safe_reports_with_prob_damp("input/day02.txt"));
     }
 }
