3d58d34eacae800bb33b1f09e2383aebfa13ed3f
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2024-12-10 00:54:04 +0000

    Day 06 part 01

--- src/aoc/day06.rs
+++ src/aoc/day06.rs
@@ -64,7 +64,8 @@ fn parse_grid(tiles_lines: Vec<String>) -> (Vec<Vec<GridEntry>>, (usize, usize))
     (tiles, (row, col))
 }
 
-fn print_grid(grid: &Vec<Vec<GridEntry>>) {
+fn print_grid(grid: &[Vec<GridEntry>]) {
+    println!("Grid:");
     for row in grid.iter() {
         for entry in row {
             match entry {
@@ -82,12 +83,71 @@ fn print_grid(grid: &Vec<Vec<GridEntry>>) {
 }
 
 fn guard_leaves_mapped_area(
-    mut_area: &mut Vec<Vec<GridEntry>>,
-    current_pos: &mut (usize, usize),
+    mut_area: &mut [Vec<GridEntry>],
+    (current_row, current_col): &mut (usize, usize),
+    current_dir: &mut Direction,
 ) -> bool {
-    mut_area[current_pos.0][current_pos.1] = GridEntry::Visited;
-    print_grid(&mut_area);
-    true
+    let current_entry = &mut mut_area[*current_row][*current_col];
+    match current_dir {
+        Direction::N => {
+            *current_entry = GridEntry::Visited;
+            if *current_row == 0 {
+                return true;
+            }
+            *current_row -= 1;
+
+            let next_entry = &mut mut_area[*current_row][*current_col];
+
+            if next_entry == &GridEntry::Obstruction {
+                *current_dir = Direction::E;
+                *current_row += 1;
+            }
+        }
+        Direction::S => {
+            *current_entry = GridEntry::Visited;
+            if *current_row == mut_area.len() - 1 {
+                return true;
+            }
+            *current_row += 1;
+
+            let next_entry = &mut mut_area[*current_row][*current_col];
+
+            if next_entry == &GridEntry::Obstruction {
+                *current_dir = Direction::W;
+                *current_row -= 1;
+            }
+        }
+        Direction::E => {
+            *current_entry = GridEntry::Visited;
+            if *current_col == mut_area[*current_row].len() - 1 {
+                return true;
+            }
+            *current_col += 1;
+
+            let next_entry = &mut mut_area[*current_row][*current_col];
+
+            if next_entry == &GridEntry::Obstruction {
+                *current_dir = Direction::S;
+                *current_col -= 1;
+            }
+        }
+        Direction::W => {
+            *current_entry = GridEntry::Visited;
+            if *current_col == 0 {
+                return true;
+            }
+            *current_col -= 1;
+
+            let next_entry = &mut mut_area[*current_row][*current_col];
+
+            if next_entry == &GridEntry::Obstruction {
+                *current_dir = Direction::N;
+                *current_col += 1;
+            }
+        }
+    }
+    //print_grid(mut_area);
+    false
 }
 
 fn get_distinct_pos(input_file: &str) -> u32 {
@@ -97,16 +157,16 @@ fn get_distinct_pos(input_file: &str) -> u32 {
 
     let mut mut_area = input.mapped_area.clone();
     let mut current_pos = input.start_pos;
+    let mut current_dir: Direction = Direction::N;
     loop {
-        if !guard_leaves_mapped_area(&mut mut_area, &mut current_pos) {
-            for i in 0..mut_area.len() {
-                for j in 0..mut_area[i].len() {
-                    if mut_area[i][j] == GridEntry::Visited {
+        if guard_leaves_mapped_area(&mut mut_area, &mut current_pos, &mut current_dir) {
+            for (row, mut_area_row) in mut_area.iter().enumerate() {
+                for (col, _) in mut_area_row.iter().enumerate() {
+                    if mut_area[row][col] == GridEntry::Visited {
                         distinct_pos += 1
                     }
                 }
             }
-        } else {
             break;
         }
     }
@@ -124,6 +184,6 @@ mod tests {
 
     #[test]
     fn test_get_distinct_pos() {
-        assert_eq!(0, get_distinct_pos("input/day06.txt"));
+        assert_eq!(5564, get_distinct_pos("input/day06.txt"));
     }
 }
