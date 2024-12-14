6d7dcb222bac09e640c6e05f833ca6cabc7078bf
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2024-12-14 20:54:57 +0000

    WIP

--- src/aoc/day06.rs
+++ src/aoc/day06.rs
@@ -67,6 +67,7 @@ fn parse_grid(grid_lines: Vec<String>) -> Grid<GridEntry> {
 }
 
 fn print_grid(grid: &Grid<GridEntry>) {
+    println!("Grid:");
     for grid_row in grid.iter_rows() {
         for grid_entry in grid_row {
             print!("{:#}", *grid_entry as u8 as char);
@@ -82,14 +83,18 @@ struct TreeNodeEntry {
     direction: Direction,
 }
 
-fn build_tree(grid: &Grid<GridEntry>, arena: &mut Arena<TreeNodeEntry>, current_node_id: NodeId) {
+fn build_tree(
+    grid: &Grid<GridEntry>,
+    arena: &mut Arena<TreeNodeEntry>,
+    current_node_id: NodeId
+) {
     let maybe_current_node = arena.get_mut(current_node_id);
     if let Some(current_node) = maybe_current_node {
         let current_node_entry = current_node.get();
         let (current_row, current_col) = current_node_entry.pos;
         if current_row as i32 - 1 < 0 {
             return;
-        }        
+        }
         if current_col as i32 - 1 < 0 {
             return;
         }
@@ -223,19 +228,72 @@ fn get_distinct_pos(input_file: &str) -> u32 {
         build_tree(&input.grid, &mut arena, root_node);
 
         let traverser = root_node.traverse(&arena);
-        let mut node_ids: HashSet<(usize, usize)> = HashSet::new();
+        let mut visited_node_ids: HashSet<(usize, usize)> = HashSet::new();
         for ev in traverser {
             match ev {
                 indextree::NodeEdge::Start(node_id) => {
                     if let Some(node) = arena.get(node_id) {
                         let node_entry = node.get();
-                        node_ids.insert(node_entry.pos);
+                        visited_node_ids.insert(node_entry.pos);
                     }
                 }
                 indextree::NodeEdge::End(_) => {}
             }
         }
-        node_ids.len() as u32
+        visited_node_ids.len() as u32
+    } else {
+        panic!("Invalid start node");
+    }
+}
+
+fn get_time_loop_pos(input_file: &str) -> u32 {
+    let input = parse_input(input_file);
+
+    let mut start_pos: Option<(usize, usize)> = None;
+    for ((row, col), pipe) in input.grid.indexed_iter() {
+        if *pipe == GridEntry::GuardN {
+            start_pos = Some((row, col));
+        }
+    }
+
+    if let Some((row, col)) = start_pos {
+        let sum_pot_time_loop_pos = input
+            .grid
+            .iter()
+            .filter(|pipe| **pipe == GridEntry::Clear)
+            .count();
+
+        let mut visited_grid_pos: HashSet<(usize, usize)> = HashSet::new();
+
+        for time_loop_pos in 0..sum_pot_time_loop_pos {
+            let mut grid = input.grid.clone();
+
+            'outer: for i in 0..grid.rows() {
+                for j in 0..grid.cols() {
+                    if let Some(grid_entry) = grid.get_mut(i, j) {
+                        if !visited_grid_pos.contains(&(i, j)) {
+                            if *grid_entry == GridEntry::Clear {
+                                *grid_entry = GridEntry::Obstruction;
+                                visited_grid_pos.insert((i, j));
+                                break 'outer;
+                            }
+                        }
+                    }
+                }
+            }
+
+            let mut arena: Arena<TreeNodeEntry> = Arena::new();
+            let root_node = arena.new_node(TreeNodeEntry {
+                grid_entry: GridEntry::GuardN,
+                pos: (row, col),
+                direction: Direction::N,
+            });
+
+            println!("Time loop pos: {}", time_loop_pos);
+            build_tree(&grid, &mut arena, root_node);
+        }
+
+        0
     } else {
         panic!("Invalid start node");
     }
@@ -252,8 +310,22 @@ mod tests {
 
     #[test]
     fn test_get_distinct_pos() {
-        stacker::grow(48 * 1024 * 1024 * 1024, || {
+        // 8MB of stack space
+        stacker::grow(8 * 1024 * 1024, || {
             assert_eq!(5564, get_distinct_pos("input/day06.txt"));
+        });
+    }
+
+    #[test]
+    fn test_get_time_loop_pos_test01() {
+        assert_eq!(6, get_time_loop_pos("input/day06_test01.txt"));
+    }
+
+    #[test]
+    fn test_get_time_loop_pos() {
+        // 8MB of stack space
+        stacker::grow(1024 * 1024 * 1024, || {
+            assert_eq!(0, get_time_loop_pos("input/day06.txt"));
         });
     }
 }
