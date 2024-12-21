2d017af10f1686de7e4fb764a19ab1f7c8971d9b
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2024-12-21 17:32:23 +0000

    WIP moving machine

--- src/aoc/day09.rs
+++ src/aoc/day09.rs
@@ -2,8 +2,12 @@
 
 use super::utils::get_lines;
 
-struct Input {
+struct InputPartOne {
     disk: Vec<DiskEntry>,
+}
+
+struct InputPartTwo {
+    disk: Vec<DiskEntryWithLen>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -18,7 +22,14 @@ struct DiskEntry {
     entry: DiskEntryType,
 }
 
-fn parse_input(input_file: &str) -> Input {
+#[derive(Debug, Clone)]
+struct DiskEntryWithLen {
+    id: Option<usize>,
+    entry: DiskEntryType,
+    len: usize,
+}
+
+fn parse_input_part_one(input_file: &str) -> InputPartOne {
     let lines = get_lines(input_file);
 
     if let Some(line) = lines.first() {
@@ -45,7 +56,39 @@ fn parse_input(input_file: &str) -> Input {
                     }
                     acc
                 });
-        Input { disk }
+        InputPartOne { disk }
+    } else {
+        panic!("Invalid input file: {}", input_file);
+    }
+}
+
+fn parse_input_part_two(input_file: &str) -> InputPartTwo {
+    let lines = get_lines(input_file);
+
+    if let Some(line) = lines.first() {
+        let chars: Vec<char> = line.chars().collect();
+
+        let mut id = 0usize;
+        let disk: Vec<DiskEntryWithLen> =
+            chars
+                .into_iter()
+                .enumerate()
+                .fold(vec![], |mut acc: Vec<DiskEntryWithLen>, (i, c)| {
+                    if i % 2 != 0 {
+                        id += 1;
+                    }
+                    acc.push(DiskEntryWithLen {
+                        id: if i % 2 == 0 { Some(id) } else { None },
+                        entry: if i % 2 == 0 {
+                            DiskEntryType::File
+                        } else {
+                            DiskEntryType::FreeSpace
+                        },
+                        len: c.to_digit(10).unwrap() as usize,
+                    });
+                    acc
+                });
+        InputPartTwo { disk }
     } else {
         panic!("Invalid input file: {}", input_file);
     }
@@ -64,16 +107,16 @@ fn find_last_file_block(blocks: &[DiskEntry]) -> Option<usize> {
 }
 
 fn has_file_block_gaps(blocks: &[DiskEntry]) -> bool {
-    let free_space_count = blocks
+    let free_space_count_at_end = blocks
         .iter()
         .rev()
         .take_while(|block| block.entry == DiskEntryType::FreeSpace)
         .count();
-    free_space_count
-        != blocks
-            .iter()
-            .filter(|block| block.entry == DiskEntryType::FreeSpace)
-            .count()
+    let free_space_count = blocks
+        .iter()
+        .filter(|block| block.entry == DiskEntryType::FreeSpace)
+        .count();
+    free_space_count_at_end != free_space_count
 }
 
 fn calc_checksum(blocks: &[DiskEntry]) -> usize {
@@ -86,12 +129,51 @@ fn calc_checksum(blocks: &[DiskEntry]) -> usize {
 }
 
 fn get_checksum(input_file: &str) -> usize {
-    let input = parse_input(input_file);
+    let input = parse_input_part_one(input_file);
 
     let mut blocks = input.disk.clone();
 
     loop {
         if has_file_block_gaps(&blocks) {
+            if let Some(first_free_space_block_pos) = find_first_free_space_block(&blocks) {
+                if let Some(last_file_block_pos) = find_last_file_block(&blocks) {
+                    let first_free_space_block = blocks[first_free_space_block_pos].clone();
+                    let last_file_block = blocks[last_file_block_pos].clone();
+
+                    blocks[first_free_space_block_pos] = last_file_block.clone();
+                    blocks[last_file_block_pos] = first_free_space_block.clone();
+                }
+            }
+        } else {
+            break;
+        }
+    }
+
+    //println!("{:?}", blocks);
+
+    calc_checksum(&blocks)
+}
+
+fn has_file_block_gaps_with_len(blocks: &[DiskEntryWithLen]) -> bool {
+    let free_space_count_at_end = blocks
+        .iter()
+        .rev()
+        .take_while(|block| block.entry == DiskEntryType::FreeSpace)
+        .fold(0, |acc, block| acc + block.len);
+    let free_space_count = blocks
+        .iter()
+        .filter(|block| block.entry == DiskEntryType::FreeSpace)
+        .fold(0, |acc, block| acc + block.len);
+    free_space_count_at_end != free_space_count
+}
+
+fn get_checksum_whole_files(input_file: &str) -> usize {
+    let input = parse_input_part_two(input_file);
+
+    let mut blocks = input.disk.clone();
+
+    loop {
+        if has_file_block_gaps_with_len(&blocks) {
             if let Some(first_free_space_block_pos) = find_first_free_space_block(&blocks) {
                 if let Some(last_file_block_pos) = find_last_file_block(&blocks) {
                     let first_free_space_block = blocks[first_free_space_block_pos].clone();
@@ -141,5 +223,18 @@ mod tests {
     #[test]
     fn test_get_checksum() {
         assert_eq!(6435922584968, get_checksum("input/day09.txt"));
+    }
+
+    #[test]
+    fn test_get_checksum_whole_files_test01() {
+        assert_eq!(2858, get_checksum_whole_files("input/day09_test01.txt"));
+    }
+
+    // This test takes a while so ignore in CI
+
+    #[ignore]
+    #[test]
+    fn test_get_checksum_whole_files() {
+        assert_eq!(0, get_checksum_whole_files("input/day09.txt"));
     }
 }
