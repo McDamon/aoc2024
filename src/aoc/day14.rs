5aa6709e3ce7e62daa9d55aab63e4983e4f9dbf4
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2024-12-27 00:40:02 +0000

    Day 14 WIP

---
+++ src/aoc/day14.rs
@@ -0,0 +1,188 @@
+// https://adventofcode.com/2024/day/13
+
+use std::collections::HashMap;
+
+use regex::Regex;
+
+use super::utils::get_lines;
+
+#[derive(Debug, Clone)]
+struct Robot {
+    pos: (i32, i32),
+    vel: (i32, i32),
+}
+
+struct Input {
+    robots: Vec<Robot>,
+}
+
+fn parse_coordinates(input: &str) -> ((i32, i32), (i32, i32)) {
+    let re = Regex::new(r"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)").unwrap();
+    let caps = re.captures(input).unwrap();
+
+    let p1 = caps.get(1).unwrap().as_str().parse::<i32>().unwrap();
+    let p2 = caps.get(2).unwrap().as_str().parse::<i32>().unwrap();
+    let v1 = caps.get(3).unwrap().as_str().parse::<i32>().unwrap();
+    let v2 = caps.get(4).unwrap().as_str().parse::<i32>().unwrap();
+
+    ((p1, p2), (v1, v2))
+}
+
+fn parse_input(input_file: &str) -> Input {
+    let lines = get_lines(input_file);
+
+    let mut robots: Vec<Robot> = Vec::new();
+
+    for line in lines {
+        let (pos, vel) = parse_coordinates(&line);
+        robots.push(Robot { pos, vel });
+    }
+
+    Input { robots }
+}
+
+fn print_robots(width: i32, height: i32, robot_map: &HashMap<(i32, i32), Vec<Robot>>) {
+    for row in 0..height {
+        for col in 0..width {
+            let mut found = false;
+            if let Some(robots) = robot_map.get(&(col, row)) {
+                print!("{}", robots.len());
+                found = true;
+            }
+            if !found {
+                print!(".");
+            }
+        }
+        println!();
+    }
+}
+
+fn get_safety_factor(input_file: &str, width: i32, height: i32, num_secs: usize) -> usize {
+    let input = parse_input(input_file);
+
+    let mut robots = input.robots.clone();
+
+    for secs in 0..num_secs {
+        for robot in &mut robots {
+            let (old_x, old_y) = robot.pos;
+            let (dx, dy) = robot.vel;
+
+            let new_x = if old_x + dx < 0 {
+                dx + width + old_x
+            } else if old_x + dx >= width {
+                dx - width + old_x
+            } else {
+                old_x + dx
+            };
+            let new_y = if old_y + dy < 0 {
+                dy + height + old_y
+            } else if old_y + dy >= height {
+                dy - height + old_y
+            } else {
+                old_y + dy
+            };
+
+            robot.pos = (new_x, new_y);
+
+            println!(
+                "Secs: {}, old_x: {}, old_y: {}, dx: {}, dy: {}, new_x: {}, new_y: {}",
+                secs + 1,
+                old_x,
+                old_y,
+                dx,
+                dy,
+                new_x,
+                new_y
+            );
+        }
+    }
+
+    let mut robot_map: HashMap<(i32, i32), Vec<Robot>> = HashMap::new();
+    for robot in robots {
+        robot_map.entry(robot.pos).or_default().push(robot);
+    }
+
+    print_robots(width, height, &robot_map);
+
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_safety_factor_test01() {
+        assert_eq!(0, get_safety_factor("input/day14_test01.txt", 11, 7, 0));
+    }
+
+    #[test]
+    fn test_get_safety_factor_test02() {
+        assert_eq!(0, get_safety_factor("input/day14_test02.txt", 11, 7, 0));
+    }
+
+    #[test]
+    fn test_get_safety_factor_test03() {
+        assert_eq!(0, get_safety_factor("input/day14_test02.txt", 11, 7, 1));
+    }
+
+    #[test]
+    fn test_get_safety_factor_test04() {
+        assert_eq!(0, get_safety_factor("input/day14_test02.txt", 11, 7, 2));
+    }
+
+    #[test]
+    fn test_get_safety_factor_test05() {
+        assert_eq!(0, get_safety_factor("input/day14_test02.txt", 11, 7, 3));
+    }
+
+    #[test]
+    fn test_get_safety_factor_test06() {
+        assert_eq!(0, get_safety_factor("input/day14_test02.txt", 11, 7, 4));
+    }
+
+    #[test]
+    fn test_get_safety_factor_test07() {
+        assert_eq!(0, get_safety_factor("input/day14_test02.txt", 11, 7, 5));
+    }
+
+    #[test]
+    fn test_get_safety_factor_test08() {
+        assert_eq!(0, get_safety_factor("input/day14_test03.txt", 11, 7, 0));
+    }
+
+    #[test]
+    fn test_get_safety_factor_test09() {
+        assert_eq!(0, get_safety_factor("input/day14_test03.txt", 11, 7, 1));
+    }
+
+    #[test]
+    fn test_get_safety_factor_test10() {
+        assert_eq!(0, get_safety_factor("input/day14_test03.txt", 11, 7, 2));
+    }
+
+    #[test]
+    fn test_get_safety_factor_test11() {
+        assert_eq!(0, get_safety_factor("input/day14_test03.txt", 11, 7, 3));
+    }
+
+    #[test]
+    fn test_get_safety_factor_test12() {
+        assert_eq!(0, get_safety_factor("input/day14_test03.txt", 11, 7, 4));
+    }
+
+    #[test]
+    fn test_get_safety_factor_test13() {
+        assert_eq!(0, get_safety_factor("input/day14_test03.txt", 11, 7, 5));
+    }
+
+    #[test]
+    fn test_get_safety_factor_test14() {
+        assert_eq!(0, get_safety_factor("input/day14_test01.txt", 11, 7, 100));
+    }
+
+    #[test]
+    fn test_get_safety_factor() {
+        assert_eq!(0, get_safety_factor("input/day14.txt", 101, 103, 100));
+    }
+}
