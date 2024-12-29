72750b48842c061197d46acdbbcd4e145a08fd89
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2024-12-29 18:21:39 +0000

    WIP

--- src/aoc/day15.rs
+++ src/aoc/day15.rs
@@ -22,10 +22,7 @@ struct Input {
 fn parse_warehouse(warehouse_part: &[&str]) -> Vec<Vec<char>> {
     warehouse_part
         .iter()
-        .map(|line| {
-            line.chars()
-                .collect()
-        })
+        .map(|line| line.chars().collect())
         .collect()
 }
 
@@ -84,11 +81,7 @@ fn get_next_move(current_pos: (usize, usize), your_move: &Move) -> (usize, usize
     }
 }
 
-fn perform_move(
-    warehouse: &mut [Vec<char>],
-    robot_pos: &mut (usize, usize),
-    your_move: &Move,
-) {
+fn perform_move(warehouse: &mut [Vec<char>], robot_pos: &mut (usize, usize), your_move: &Move) {
     //println!("Move: {:?}", your_move);
     let (robot_x, robot_y) = robot_pos;
     let mut maybe_next_move = Some(get_next_move((*robot_x, *robot_y), your_move));
@@ -167,37 +160,71 @@ fn perform_move_wider(
     robot_pos: &mut (usize, usize),
     your_move: &Move,
 ) {
+    println!("Move: {:?}", your_move);
+    let (robot_x, robot_y) = robot_pos;
+    let mut maybe_next_move = Some(get_next_move((*robot_x, *robot_y), your_move));
+    while let Some(next_move) = maybe_next_move {
+        let (next_x, next_y) = next_move;
+        match warehouse[next_y][next_x] {
+            '.' => {
+                warehouse[*robot_y][*robot_x] = '.';
+                warehouse[next_y][next_x] = '@';
+                *robot_x = next_x;
+                *robot_y = next_y;
+                maybe_next_move = None;
+            }
+            'O' => {
+                let (peek_x, peek_y) = get_next_move((next_x, next_y), your_move);
+                match warehouse[peek_y][peek_x] {
+                    '.' => {
+                        warehouse[next_y][next_x] = '.';
+                        warehouse[peek_y][peek_x] = 'O';
+                        maybe_next_move = Some(get_next_move((*robot_x, *robot_y), your_move))
+                    }
+                    'O' => maybe_next_move = Some((peek_x, peek_y)),
+                    _ => maybe_next_move = None,
+                }
+            }
+            _ => {
+                maybe_next_move = None;
+            }
+        }
+    }
+
+    print_warehouse(warehouse);
+    println!();
 }
 
 fn get_sum_gps_wider(input_file: &str) -> u32 {
     let input = parse_input(input_file);
 
-    let mut warehouse_wider: Vec<Vec<char>> = input.warehouse.iter().fold(vec![], |mut acc, row| {
-        let mut new_row: Vec<char> = vec![];
-        for entry in row {
-            match entry {
-                ' ' => {
-                    new_row.push('.');
-                    new_row.push('.');
+    let mut warehouse_wider: Vec<Vec<char>> =
+        input.warehouse.iter().fold(vec![], |mut acc, row| {
+            let mut new_row: Vec<char> = vec![];
+            for entry in row {
+                match entry {
+                    '.' => {
+                        new_row.push('.');
+                        new_row.push('.');
+                    }
+                    '#' => {
+                        new_row.push('#');
+                        new_row.push('#');
+                    }
+                    'O' => {
+                        new_row.push('[');
+                        new_row.push(']');
+                    }
+                    '@' => {
+                        new_row.push('@');
+                        new_row.push('.');
+                    }
+                    _ => panic!("Unknown entry"),
                 }
-                '#' => {
-                    new_row.push('#');
-                    new_row.push('#');
-                }
-                'O' => {
-                    new_row.push('[');
-                    new_row.push(']');
-                }
-                '@' => {
-                    new_row.push('@');
-                    new_row.push('.');
-                }
-                _ => panic!("Unknown entry"),
             }
-        }
-        acc.push(new_row);
-        acc
-    });
+            acc.push(new_row);
+            acc
+        });
 
     let mut robot_pos: (usize, usize) = get_robot_pos(&warehouse_wider);
 
