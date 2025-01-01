dab92f8f4bc5e68047a81feea59e7596c5bbb707
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2025-01-01 00:01:23 +0000

    More gradual WIP

--- src/aoc/day15.rs
+++ src/aoc/day15.rs
@@ -74,13 +74,13 @@ fn print_warehouse(warehouse: &[Vec<char>]) {
     }
 }
 
-fn get_next_move(curr_pos: (usize, usize), move_dir: &Move) -> (usize, usize) {
-    let (curr_x, curr_y) = curr_pos;
+fn get_next_move(move_pos: (usize, usize), move_dir: &Move) -> (usize, usize) {
+    let (move_x, move_y) = move_pos;
     match move_dir {
-        Move::Up => (curr_x, curr_y - 1),
-        Move::Down => (curr_x, curr_y + 1),
-        Move::Left => (curr_x - 1, curr_y),
-        Move::Right => (curr_x + 1, curr_y),
+        Move::Up => (move_x, move_y - 1),
+        Move::Down => (move_x, move_y + 1),
+        Move::Left => (move_x - 1, move_y),
+        Move::Right => (move_x + 1, move_y),
     }
 }
 
@@ -191,59 +191,29 @@ fn perform_move_wider(
     warehouse: &mut [Vec<char>],
     robot_pos: &mut (usize, usize),
     move_dir: &Move,
-) {
-    println!("Move: {:?}", move_dir);
-    let (robot_x, robot_y) = robot_pos;
-    let mut maybe_next_move = Some(get_next_move((*robot_x, *robot_y), move_dir));
-    while let Some(next_move) = maybe_next_move {
-        let (next_x, next_y) = next_move;
-        match warehouse[next_y][next_x] {
-            '.' => {
-                warehouse[*robot_y][*robot_x] = '.';
-                warehouse[next_y][next_x] = '@';
-                *robot_x = next_x;
-                *robot_y = next_y;
-                maybe_next_move = None;
-            }
-            '[' | ']' => {
-                maybe_next_move =
-                    perform_move_box_wider(warehouse, (*robot_x, *robot_y), next_move, move_dir);
-            }
-            _ => {
-                maybe_next_move = None;
-            }
-        }
-    }
-
-    print_warehouse(warehouse);
-    println!();
-}
-
-fn perform_move_box_wider(
-    warehouse: &mut [Vec<char>],
-    robot_pos: (usize, usize),
-    first_box_pos: (usize, usize),
-    move_dir: &Move,
 ) -> Option<(usize, usize)> {
-    let mut move_queue: VecDeque<(usize, usize)> = VecDeque::new();
-    move_queue.push_front(first_box_pos);
+    println!("Move: {:?}", move_dir);
 
+    // Stores indices of warehouse cells for BFS
+    let mut move_queue: VecDeque<(usize, usize)> = VecDeque::new();
+
+    // Stores visited cells for BFS
     let mut visited_moves: HashSet<(usize, usize)> = HashSet::new();
 
-    while let Some(curr_move_pos) = move_queue.pop_front() {
-        if let Some(adj_moves) = get_adj_moves(curr_move_pos, move_dir) {
+    // Mark the starting cell as visited and push it to the queue
+    visited_moves.insert(*robot_pos);
+    move_queue.push_front(*robot_pos);
+
+    while let Some(move_pos) = move_queue.pop_front() {
+        if let Some(adj_moves) = get_adj_moves(move_pos, move_dir) {
             for adj_move_pos in &adj_moves {
-                let (curr_move_x, curr_move_y) = curr_move_pos;
-                let curr_move = warehouse[curr_move_y][curr_move_x];
-                if is_box_move_possible(warehouse, &visited_moves, curr_move_pos) {
+                if is_box_move_possible(warehouse, &visited_moves, *adj_move_pos) {
                     let (adj_move_x, adj_move_y) = adj_move_pos;
                     let adj_move = warehouse[*adj_move_y][*adj_move_x];
                     println!(
                         "Visiting box {:?} at {:?}, move_dir: {:?}",
                         adj_move, adj_move_pos, move_dir
                     );
-
-                    warehouse[*adj_move_y][*adj_move_x] = curr_move;
 
                     move_queue.push_front(*adj_move_pos);
                     visited_moves.insert(*adj_move_pos);
@@ -253,53 +223,56 @@ fn perform_move_box_wider(
             continue;
         }
     }
-    Some(robot_pos)
+
+    print_warehouse(warehouse);
+    println!();
+
+    Some(*robot_pos)
 }
 
 fn is_box_move_possible(
     warehouse: &[Vec<char>],
     visited_moves: &HashSet<(usize, usize)>,
-    curr_move_pos: (usize, usize),
+    move_pos: (usize, usize),
 ) -> bool {
-    let (curr_move_x, curr_move_y) = curr_move_pos;
+    let (move_x, move_y) = move_pos;
 
     // If cell is out of bounds
-    if curr_move_x >= warehouse[0].len() || curr_move_y >= warehouse.len() {
+    if move_x >= warehouse[0].len() || move_y >= warehouse.len() {
         return false;
     }
 
     // If cell is visited
-    if visited_moves.contains(&curr_move_pos) {
+    if visited_moves.contains(&move_pos) {
         return false;
     }
 
-    // Can't be a wall or a dot
-    match warehouse[curr_move_y][curr_move_x] {
-        '#' | '.' => return false,
-        _ => (),
+    // If cell is a wall
+    if warehouse[move_y][move_x] == '#' {
+        return false;
     }
 
     // Otherwise can be visited
     return true;
 }
 
-fn get_adj_moves(curr_pos: (usize, usize), move_dir: &Move) -> Option<Vec<(usize, usize)>> {
-    let (curr_x, curr_y) = curr_pos;
+fn get_adj_moves(move_pos: (usize, usize), move_dir: &Move) -> Option<Vec<(usize, usize)>> {
+    let (move_x, move_y) = move_pos;
     let mut adj_moves = vec![];
 
     let potential_moves = match move_dir {
         Move::Up => vec![
-            (curr_x - 1, curr_y - 1),
-            (curr_x, curr_y - 1),
-            (curr_x + 1, curr_y - 1),
+            (move_x - 1, move_y - 1),
+            (move_x, move_y - 1),
+            (move_x + 1, move_y - 1),
         ],
         Move::Down => vec![
-            (curr_x - 1, curr_y + 1),
-            (curr_x, curr_y + 1),
-            (curr_x + 1, curr_y + 1),
+            (move_x - 1, move_y + 1),
+            (move_x, move_y + 1),
+            (move_x + 1, move_y + 1),
         ],
-        Move::Left => vec![(curr_x - 1, curr_y)],
-        Move::Right => vec![(curr_x + 1, curr_y)],
+        Move::Left => vec![(move_x - 1, move_y)],
+        Move::Right => vec![(move_x + 1, move_y)],
     };
 
     for potential_move in potential_moves {
